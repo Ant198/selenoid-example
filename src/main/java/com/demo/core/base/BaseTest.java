dfdb99525944182fd5c178a25bd1c78be531ce6e
Anton <ridnovir@yahoo.com>
2025-04-14 14:18:55 +0300

    edite

--- src/main/java/com/demo/core/base/BaseTest.java
+++ src/main/java/com/demo/core/base/BaseTest.java
@@ -18,7 +18,7 @@ public class BaseTest extends AllureLogger {
         String url = String.format(Constants.URL, "MNST/monster-beverage");
         logInfo("Creating web driver configuration..."); //test
         SelenideConfig.createBrowserConfig(System.getProperty("selenide.browser", "chrome"));
-        Configuration.remote = System.getenv("SELENOID_URL");
+        Configuration.remote = "http://selenoid:4444/wd/hub";
         configLog(this.getClass().getSimpleName());
         logInfo("Open browser...");
         Selenide.open(url);
